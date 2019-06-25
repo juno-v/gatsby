8a0833079b00b83208bf0703b10450763f38a9cd
Juno Vue <Juno.Vue@Hotmail.com>
2019-06-25 02:09:58 -0500

    created style folder to utilize CSS

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Footer from "./footer"; 
 import Header from "./header"; 
+import '../styles/index.css';
 
 // componentizing this to enable editing the layout of the page in one file versus having to go into every individual file and editing
 const Layout = (props) => {
