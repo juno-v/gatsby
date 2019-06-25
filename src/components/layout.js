76407d140c69864f44d1cddb170a2fada85639a3
Juno Vue <Juno.Vue@Hotmail.com>
2019-06-25 02:16:53 -0500

    changed styles index.css to index.scss color changes display on DOM. confirms scss functionality

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Footer from "./footer"; 
 import Header from "./header"; 
-import '../styles/index.css';
+import '../styles/index.scss';
 
 // componentizing this to enable editing the layout of the page in one file versus having to go into every individual file and editing
 const Layout = (props) => {
