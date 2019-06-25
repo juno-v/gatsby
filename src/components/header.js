a33f92c3875be359ed49faca186b3db25e8dd2d1
Juno Vue <Juno.Vue@Hotmail.com>
2019-06-25 16:38:03 -0500

    changed header.scss to a css module to utilize local styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'gatsby'; 
-import './header.scss'
+import './header.module.scss'
 
 const Header = () => {
     return (
@@ -10,7 +10,7 @@ const Header = () => {
                 <nav> 
                     <ul>
                         <li>
-                            <Link to="/"> Home </Link>
+                            <Link className="link" to="/"> Home </Link>
                         </li>
                         <li>
                             <Link to="/blog"> Blog </Link>
