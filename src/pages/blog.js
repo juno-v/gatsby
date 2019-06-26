b52afa3d318eb2403fd4470d2cfefe5f9bd61691
Juno Vue <Juno.Vue@Hotmail.com>
2019-06-25 23:53:56 -0500

    queried md files. return object with wanted contents in a console.log

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,8 +1,25 @@
 import React from 'react'; 
 import Layout from "../components/layout"; 
+import { graphql, useStaticQuery} from 'gatsby'; 
 
 
 const BlogPage = () => {
+
+    const data = useStaticQuery(graphql`
+        query {
+            allMarkdownRemark {
+                edges {
+                    node {
+                    frontmatter {
+                            title
+                            date
+                        }
+                    }
+                }
+            }
+        }
+    `)
+
     return (
         <Layout>
             <h1>Welcome to my blogs!</h1>
