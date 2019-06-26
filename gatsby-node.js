d7f9d4d57672f7dfd519349039bc64ff0ed21ab0
Juno Vue <Juno.Vue@Hotmail.com>
2019-06-26 17:46:16 -0500

    able to generate new pages to slug endpoint

--- gatsby-node.js
+++ gatsby-node.js
@@ -40,3 +40,34 @@ module.exports.onCreateNode = ({node, actions}) => {
         })
     }
 }
+
+module.exports.createPages = async ({graphql, actions}) => {
+    const{ createPage } = actions; 
+
+    // get path to template
+    const blogTemplate = path.resolve('./src/templates/blog.js'); 
+    // get markdown data
+    const response = await graphql(`
+        query {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        fields {
+                            slug
+                        }
+                    }
+                }
+            }
+        }
+    `)
+    // create new pages
+    response.data.allMarkdownRemark.edges.forEach((edge) => {
+        createPage({
+            component: blogTemplate,
+            path: `/blog/${edge.node.fields.slug}`,
+            context: {
+                slug: edge.node.fields.slug
+            }
+        })
+    }) 
+}
--- src/templates/blog.js
+++ src/templates/blog.js
@@ -3,6 +3,10 @@
 import React, { Component } from 'react';
 import Layout from "../components/layout";
 
+/* notes about createPage below */
+// Tell plugins to add pages. This extension point is called only after the initial sourcing and 
+// transformation of nodes plus creation of the GraphQL schema are complete so you can query your data in order to create pages.
+
 const Blog = () => {
     return (
         <Layout>
