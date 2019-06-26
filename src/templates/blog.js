7fe6f9712287e794dfaeacb1214be8a7a55dee98
Juno Vue <Juno.Vue@Hotmail.com>
2019-06-26 18:01:20 -0500

    able to fetch query in src/templates.blog.js. Title renders onto the DOM.

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,16 +1,38 @@
-// template for blog post pages 
+// template for blog post pages from /src/pages/blog.js
 
 import React, { Component } from 'react';
+import { graphql } from 'gatsby'; 
 import Layout from "../components/layout";
 
 /* notes about createPage below */
 // Tell plugins to add pages. This extension point is called only after the initial sourcing and 
 // transformation of nodes plus creation of the GraphQL schema are complete so you can query your data in order to create pages.
 
-const Blog = () => {
+
+export const query = graphql`
+    query  (
+        $slug: String!
+    ) {
+        markdownRemark (
+        fields: {
+            slug: {
+            eq: $slug
+            }
+        }
+        ) {
+        frontmatter {
+            title
+            date
+            }
+            html
+        }
+    }
+`
+
+const Blog = (props) => {
     return (
         <Layout>
-            This is the blog template 
+            <h1> {props.data.markdownRemark.frontmatter.title} </h1>
         </Layout>
     )
 }
