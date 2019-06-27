39729c30084823fe6f130e8ffd6d98e7848d6581
Juno Vue <Juno.Vue@Hotmail.com>
2019-06-26 21:38:39 -0500

    adjusted code in src/templates.blog.js to utilize Contentful instead of mark down files. left comments for mark down queries and render.

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -8,23 +8,35 @@ import Layout from "../components/layout";
 // Tell plugins to add pages. This extension point is called only after the initial sourcing and 
 // transformation of nodes plus creation of the GraphQL schema are complete so you can query your data in order to create pages.
 
+/* code to query markdown files */
+
+// export const query = graphql`
+//     query  (
+//         $slug: String!
+//     ) {
+//         markdownRemark (
+//         fields: {
+//             slug: {
+//             eq: $slug
+//             }
+//         }
+//         ) {
+//         frontmatter {
+//             title
+//             date
+//             }
+//             html
+//         }
+//     }
+// `
+
+/* code to query Contentful */
 
 export const query = graphql`
-    query  (
-        $slug: String!
-    ) {
-        markdownRemark (
-        fields: {
-            slug: {
-            eq: $slug
-            }
-        }
-        ) {
-        frontmatter {
+    query($slug: String!) {
+        contentfulBlogPost(slug: {eq: $slug}) {
             title
-            date
-            }
-            html
+            publishedDate(formatString: "MMMM, Do, YYY")
         }
     }
 `
@@ -32,11 +44,20 @@ export const query = graphql`
 const Blog = (props) => {
     return (
         <Layout>
-            <h1> {props.data.markdownRemark.frontmatter.title} </h1>
+            {/* this is the code to utilize mark down files */}
+
+            {/* <h1> {props.data.markdownRemark.frontmatter.title} </h1>
             <p> {props.data.markdownRemark.frontmatter.date} </p>
             {/* don't leave a space in the div elements, will cause error:
             Can only set one of `children` or `props.dangerouslySetInnerHTML`. */}
-            <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}></div>
+            {/* <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}></div>  */}
+
+
+            {/* this is the code to utilize Contentful content */}
+
+            <h1>{props.data.contentfulBlogPost.title} </h1>
+            <p> {props.data.contentfulBlogPost.publishedDate} </p>
+
         </Layout>
     )
 }
