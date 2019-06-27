1a91a30d59087783a79a4c287b2d50bc72066f31
Juno Vue <Juno.Vue@Hotmail.com>
2019-06-26 21:44:52 -0500

    installed package to fetch body from edges in a more accessible way

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import { graphql } from 'gatsby'; 
 import Layout from "../components/layout";
 
+
 /* notes about createPage below */
 // Tell plugins to add pages. This extension point is called only after the initial sourcing and 
 // transformation of nodes plus creation of the GraphQL schema are complete so you can query your data in order to create pages.
