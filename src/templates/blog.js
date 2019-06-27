a60e8a6c1bb0376dcc834843c705ea5c4d0aa59d
Juno Vue <Juno.Vue@Hotmail.com>
2019-06-26 23:31:31 -0500

    minor mis spell in pubslishedDate query. displayed the wrong format

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -38,7 +38,7 @@ export const query = graphql`
     query($slug: String!) {
         contentfulBlogPost(slug: {eq: $slug}) {
             title
-            publishedDate(formatString: "MMMM, Do, YYY")
+            publishedDate(formatString: "MMMM, Do, YYYY")
             body {
                 json
             }
