414856f759e43a20262a26664ab86d3b0af74cef
Juno Vue <Juno.Vue@Hotmail.com>
2019-06-26 22:43:43 -0500

    queried site title for the tab display.

--- src/components/head.js
+++ src/components/head.js
@@ -1,9 +1,22 @@
 import React from 'react'; 
 import { Helmet } from 'react-helmet'; 
+import { useStaticQuery, graphql } from 'gatsby'; 
+
 
 const Head = () => {
+
+    const data = useStaticQuery(graphql`
+        query {
+            site {
+                siteMetadata {
+                    title
+                }
+            }
+        }
+    `)
+
     return (
-        <Helmet title="This is a test" />
+        <Helmet title={data.site.siteMetadata.title} />
     )
 }
 
