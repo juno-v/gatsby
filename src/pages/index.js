00c19a81a8964c3cfc073c31735c81943c39d337
Juno Vue <Juno.Vue@Hotmail.com>
2019-06-27 23:57:39 -0500

    added some images to use on home page. removed <p> tag that ul was nested in and unused Link import.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react'; 
-import { Link } from 'gatsby'; 
 import Layout from "../components/layout"; 
 import Head from "../components/head"; 
 import Andrew from "./images/AndrewMead.jpg"
@@ -15,14 +14,14 @@ const IndexPage = () => {
             <Head title="Home" /> 
             <h1> Welcome to the Gatsby Blogs! </h1>
             <h2> This is a website that I've created using: </h2>
-                <p>
+                
                     <ul>
-                        <li> GatsbyJS </li>
+                        <li> GatsbyJS </li> 
                         <li> GraphQL </li>
                         <li> Contentful </li>
                         <li> Netlify </li>
                     </ul>
-                </p>
+                
                 <p> I followed along Andrew Mead's  
                     <a href="https://www.youtube.com/watch?v=8t0vNu2fCCM&t=4122s"
                     target="blank"
@@ -31,7 +30,7 @@ const IndexPage = () => {
                 <img 
                 className={indexStyles.andrewImg}
                 src={Andrew} 
-                atl="Andrew Mead"/>
+                alt="Andrew Mead"/>
                 <p> Check out my Blog section to read what I think about Gatsby and my experience with Mead's tutorial. </p>
         </Layout>
     )
