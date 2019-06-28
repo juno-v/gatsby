733519a01c835e79d945f5b043c7e395349377e8
Juno Vue <Juno.Vue@Hotmail.com>
2019-06-28 00:17:31 -0500

    edited about page.

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,8 +8,11 @@ const AboutPage = () => {
     return (
         <Layout>
             <Head title="About" />
-            <h1> About Page </h1>
-            <p> About informatino will be in here... </p>
+            <p> I like to try new technologies through tutorials and research.
+                I typically deploy my applications onto a website where people are able to demo the application. 
+                I also make my Github repos public so others can view them. 
+                There are comments in my repos for others to read through to give them an easier understanding of my code.
+            </p>
             <p> <Link to="contact"> Want to work with me? Reach out!</Link></p>
         </Layout>
     )
