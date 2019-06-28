dc66b555a61e6e7ffdf8fd6efc2f4bae0ab6f8f9
Juno Vue <Juno.Vue@Hotmail.com>
2019-06-28 00:56:10 -0500

    updated contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,8 +7,15 @@ const ContactPage = () => {
     return (
         <Layout>
             <Head title="Contact" />
-            <h1> Contact Page! </h1>
-            <p>This is my twitter. <a href="https://twitter.com/Juno73172572" target="blank"> @JunoVue </a></p>
+            <p> I create youtube videos. </p>
+            <p> These videos include code tutorials, tips/advice on developer learning techniques, 
+                job search, personal experiences, and more! 
+            </p>
+            <p> There is a Slack work space for my viewers and I. </p>
+            <p> Slack is a communication source where users can message each other for tools, files, code snipppets, and converse in comments and threads.</p>
+            <p>  It's a great way for everyone to collaborate!</p>  
+            <p> You can always find an updated link to join the Slack workspace <a href="https://www.youtube.com/" target="blank" 
+            > here </a> </p>
         </Layout>
 
         
