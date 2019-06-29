1443189fa8fbf76b19bd4843a4049e4166431d9b
Juno Vue <Juno.Vue@Hotmail.com>
2019-06-29 17:25:54 -0500

    changed information in about me section to utilize unordered list.

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,11 +8,14 @@ const AboutPage = () => {
     return (
         <Layout>
             <Head title="About" />
-            <p> I like to try new technologies through tutorials and research.
-                I typically deploy my applications onto a website where people are able to demo the application. 
-                I also make my Github repos public so others can view them. 
-                There are comments in my repos for others to read through to give them an easier understanding of my code.
-            </p>
+            <p> Hello world! Thank you for stopping by the website. My name is Juno. </p>
+            <p> Here's a little bit about me: </p>
+            <ul>
+                <li> I like to try new technologies through tutorials and research. </li>
+                <li>  I typically deploy my applications onto a website where people are able to demo the application.  </li>
+                <li>  I also make my Github repos public so others can view them.  </li>
+                <li> There are comments in my repos for others to read through to give them an easier understanding of my code. </li>
+            </ul>
             <p> Want to work with me? Read my <Link to="contact"> contact page </Link> and reach out! </p>
         </Layout>
     )
