fc3a9718f51f845de077a8d5fdb4661223ea2e1c
Juno Vue <Juno.Vue@Hotmail.com>
2019-06-29 17:38:37 -0500

    added styles to a p tag nested in Github list item.

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'gatsby'; 
 import Layout from "../components/layout"; 
 import Head from "../components/head"; 
+import aboutStyles from "./about.module.scss"
 
 
 const AboutPage = () => {
@@ -10,11 +11,15 @@ const AboutPage = () => {
             <Head title="About" />
             <p> Hello world! Thank you for stopping by the website. My name is Juno. </p>
             <p> Here's a little bit about me: </p>
-            <ul>
+            <ul className={aboutStyles.ul} >
                 <li> I like to try new technologies through tutorials and research. </li>
                 <li>  I typically deploy my applications onto a website where people are able to demo the application.  </li>
-                <li>  I also make my <a href="https://github.com/jvue96" target="blank"> Github </a> repos public so others can view them.  </li>
-                <li> There are comments in my repos for others to read through to give them an easier understanding of my code. </li>
+                <li>  I also make my 
+                    <a href="https://github.com/jvue96" target="blank"> Github </a> 
+                    repos public so others can view them.  
+                    <p> There are comments in my repos for others to read through to give them an easier understanding of my code.  </p>
+                    </li>
+                
             </ul>
             <p> Want to work with me? Read my <Link to="contact"> contact page </Link> and reach out! </p>
         </Layout>
